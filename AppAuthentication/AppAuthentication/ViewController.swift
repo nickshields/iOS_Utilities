0f6d5af709c2a38078b402e212e78101703e50fb
Nick Shields <Nick_Shields@ultimatesoftware.com>
2020-04-07 09:56:13 -0400

    Firebase auth work

--- AppAuthentication/AppAuthentication/ViewController.swift
+++ AppAuthentication/AppAuthentication/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import AuthenticationServices
+import FirebaseAuth
+import CryptoKit
 
 @IBDesignable class ViewController: UIViewController {
     
@@ -25,6 +27,13 @@ import AuthenticationServices
         stackView.spacing = 20
         setStackViewConstraints()
         addButtonsToStackView()
+    }
+    
+    func setStackViewConstraints(){
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
     }
     
     func addButtonsToStackView(){
@@ -79,18 +88,59 @@ import AuthenticationServices
         
     }
     
-    func setStackViewConstraints(){
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-        stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    //FIREBASE AUTHENTICATION START
+    
+    //MARK: 1. Generate Nonce for sign in
+    
+    private func randomNonceString(length: Int = 32) -> String{
+        precondition(length > 0)
+        let charset: Array<Character> = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
     }
     
+    private func sha256(_ input: String) -> String{
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        }.joined()
+        return hashString
+    }
+    
+    fileprivate var currentNonce: String?
+    
+    //MARK: Start Apple Sign in Flow: (Do it when they push the button)
     @objc
     func didTapAppleButton(){
+        let nonce = randomNonceString()
+        currentNonce = nonce
         let provider = ASAuthorizationAppleIDProvider()
         let request = provider.createRequest()
         request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
         
         let controller = ASAuthorizationController(authorizationRequests: [request])
         controller.delegate = self
@@ -98,6 +148,13 @@ import AuthenticationServices
         
         controller.performRequests()
     }
+    
+    
+    //FIREBASE AUTHENTICATION FINISH
+    
+
+    
+
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         if let mainVC = segue.destination as? MainViewController, let user =
@@ -109,6 +166,34 @@ import AuthenticationServices
 
 extension ViewController: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+          guard let nonce = currentNonce else {
+            fatalError("Invalid state: A login callback was received, but no login request was sent.")
+          }
+          guard let appleIDToken = appleIDCredential.identityToken else {
+            print("Unable to fetch identity token")
+            return
+          }
+          guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+            print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+            return
+          }
+          // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+          // Sign in with Firebase.
+          Auth.auth().signIn(with: credential) { (authResult, error) in
+            if (error != nil) {
+              // Error. If error.code == .MissingOrInvalidNonce, make sure
+              // you're sending the SHA256-hashed nonce as a hex string with
+              // your request to Apple.
+                print(error!.localizedDescription)
+                return
+            }
+            // User is signed in to Firebase with Apple.
+            // ...
+            print(authResult!)
+          }
+        }
         
         switch authorization.credential {
             
